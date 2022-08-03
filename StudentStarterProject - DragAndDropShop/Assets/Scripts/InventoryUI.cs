eebefcdfee70880a21c94a397f8209c1bc39f9b8
julian-pahor <julian.pahor1998@gmail.com>
2022-08-03 15:59:33 +0930

    Drag+Drop - dragging along

--- StudentStarterProject - DragAndDropShop/Assets/Scripts/InventoryUI.cs
+++ StudentStarterProject - DragAndDropShop/Assets/Scripts/InventoryUI.cs
@@ -15,6 +15,16 @@ public class InventoryUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        slots = new Slot[inventory.items.Length];
 
+        for (int i = 0; i < inventory.items.Length; i++)
+        {
+            slots[i] = Instantiate(slotPrefab, transform);
+            //create a shop item UI and feed the item to it
+            slots[i].itemUI = Instantiate(itemPrefab, slots[i].transform);
+            slots[i].itemUI.SetItem(inventory.items[i]);
+            //create a slot
+            slots[i].Init(this, i, slots[i].itemUI);
+        }
     }
 }
--- StudentStarterProject - DragAndDropShop/Assets/Scripts/ShopItemUI.cs
+++ StudentStarterProject - DragAndDropShop/Assets/Scripts/ShopItemUI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ShopItemUI : MonoBehaviour
+public class ShopItemUI : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     public Image image;
 
@@ -43,5 +43,48 @@ public class ShopItemUI : MonoBehaviour
         }
     }
 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (originalParent == null) originalParent = transform.parent;
+        if (canvas == null) canvas = GetComponentInParent<Canvas>();
 
+        transform.SetParent(canvas.transform, true);
+        transform.SetAsLastSibling();
+
+        dragging = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (dragging)
+        {
+            transform.position = eventData.position;
+        }
+    }
+
+    List<RaycastResult> hits = new List<RaycastResult>();
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //is there a slot underneath us?
+        Slot slotFound = null;
+        EventSystem.current.RaycastAll(eventData, hits);
+
+        foreach (RaycastResult hit in hits)
+        {
+            Slot s = hit.gameObject.GetComponent<Slot>();
+            if(s)
+            {
+                slotFound = s;
+            }
+        }
+
+        if(slotFound)
+        {
+            Swap(slotFound);
+        }
+
+        transform.SetParent(originalParent, true);
+        transform.localPosition = Vector3.zero;
+    }
 }
